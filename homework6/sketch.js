76eaf4d821130cfdae4b41c9a0b9bb7dcf8fa9b3
Sophie Guilliams <157538151+Sophienoel12@users.noreply.github.com>
2026-02-23 15:15:46 -0700

    Add files via upload

---
+++ homework6/sketch.js
@@ -0,0 +1,82 @@
+let idleFrames = [];
+let walkFrames = [];
+
+let currentAnimation = [];
+let frameIndex = 0;
+let frameDelay = 10; // controls animation speed
+let frameCounter = 0;
+
+let x = 100;
+let y = 300;
+let speed = 2;
+
+let foods = [];
+
+function preload() {
+  // Idle animation
+  idleFrames[0] = loadImage("images/idle1.png");
+  idleFrames[1] = loadImage("images/idle2.png");
+
+  // Walk animation
+  walkFrames[0] = loadImage("images/walk1.png");
+  walkFrames[1] = loadImage("images/walk2.png");
+}
+
+function setup() {
+  createCanvas(800, 600);
+
+  currentAnimation = idleFrames;
+
+  // Create 5 food objects
+  foods.push(new Food(100, 100, 40, color(255, 0, 0)));
+  foods.push(new Food(250, 200, 60, color(0, 255, 0)));
+  foods.push(new Food(400, 150, 50, color(0, 0, 255)));
+  foods.push(new Food(550, 300, 70, color(255, 255, 0)));
+  foods.push(new Food(700, 400, 45, color(255, 0, 255)));
+}
+
+function draw() {
+  background(220);
+
+  // Display all food objects
+  for (let i = 0; i < foods.length; i++) {
+    foods[i].display();
+  }
+
+  handleMovement();
+  animateCharacter();
+}
+
+function handleMovement() {
+  let moving = false;
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += speed;
+    moving = true;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= speed;
+    moving = true;
+  }
+
+  // Switch animation
+  if (moving) {
+    currentAnimation = walkFrames;
+  } else {
+    currentAnimation = idleFrames;
+  }
+}
+
+function animateCharacter() {
+  frameCounter++;
+
+  if (frameCounter >= frameDelay) {
+    frameCounter = 0;
+    frameIndex++;
+    if (frameIndex >= currentAnimation.length) {
+      frameIndex = 0; // Loop animation
+    }
+  }
+
+  image(currentAnimation[frameIndex], x, y);
+}
